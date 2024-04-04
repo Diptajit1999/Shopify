23b02826fa1dcca3bc309002e8625d097c97553d
Diptajit Das <diptajitdas66@gmail.com>
2024-04-04 21:08:34 +0530

    putting logic

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,11 +1,52 @@
 //implementing product logic
+const { ProductModel } = require("../models/product.model");
+
+// Get All Products
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await ProductModel.find();
+    res.status(201).send({ msg: "Getting all products", products });
+  } catch (error) {
+    console.log("Error in getting all products:", error);
+    res.status(500).send({ msg: "Internal Server Error" });
+  }
+};
+
+// Creating or adding a new product ---> admin acess only
+const createProduct = async (req, res) => {
+  const payload = req.body;
+
+  try {
+    const product = new ProductModel(payload);
+    await product.save();
+    res.status(201).send({ msg: "A new product has been added", product });
+  } catch (error) {
+    console.error("Error in creating product:", error);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+};
 
 
-const getAllProducts=(req,res)=>{
-    res.status(200).send("get all products")
+// Updating a product ---> admin access only
+const updateProduct=async(req,res)=>{
+    const productId=req.params.id;
+    const updates=req.body
+
+    
+    try {
+        let product=await ProductModel.findById(productId)
+
+        if(!product){
+            return res.status(400).send({msg:"Product not found"})
+        }
+        const updatedProduct=await ProductModel.findByIdAndUpdate(productId,updates,{new:true})
+        res.status(201).send({ msg: "A new product has been added", updatedProduct });
+    } catch (error) {
+        console.error("Error in updating product:", error);
+    res.status(500).send({ error: "Internal Server Error" });
+    }
 }
-
-
-module.exports={
-    getAllProducts
-}
+module.exports = {
+  getAllProducts,
+  createProduct,
+};
